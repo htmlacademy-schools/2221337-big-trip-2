7de962cc23258784188867b4cb3b0a4c828e3b62
Pavel Vakhrin <ratchet23122003@gmail.com>
2023-03-17 01:07:26 +0500

    3.12. Революция или эволюция? (часть 2)

--- src/view/trip-event-view.js
+++ src/view/trip-event-view.js
@@ -26,7 +26,7 @@ const createTripEventTemplate = (tripEvent, offersByType) => {
         <div class="event__type">
           <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
         </div>
-        <h3 class="event__title">${uppperFirstSymbol(type).concat(' ', destination.name)}</h3>
+        <h3 class="event__title">${uppperFirstSymbol(type)} ${destination.name}</h3>
         <div class="event__schedule">
           <p class="event__time">
             <time class="event__start-time" datetime="${humanizeEventTime(dateFrom, 'YYYY-MM-DD[T]HH:mm')}">${humanizeEventTime(dateFrom, 'HH:mm')}</time>
